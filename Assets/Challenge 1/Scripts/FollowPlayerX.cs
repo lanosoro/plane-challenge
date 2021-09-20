b8f7eeb4cfacc2e536f724bf1ae387f4cc9c4a62
Ianosoro <43702447+lanosoro@users.noreply.github.com>
2021-09-20 19:43:01 +0300

    intitial commit

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(20,4,0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
